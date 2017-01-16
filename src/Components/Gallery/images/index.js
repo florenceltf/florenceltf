a69897b850ad4583bb709711b40945d1dbcc7d51
Eamon Taaffe <eamontaaffe@gmail.com>
2017-01-16 21:44:55 +1100

    added new pics

--- src/Components/Gallery/images/index.js
+++ src/Components/Gallery/images/index.js
@@ -2,6 +2,6 @@ import Image1 from './THUMB_1.JPG';
 import Image2 from './THUMB_2.JPG';
 import Image3 from './THUMB_3.JPG';
 import Image4 from './THUMB_4.JPG';
-import Image5 from './THUMB_5.JPG';
+// import Image5 from './THUMB_5.JPG';
 
-export default [Image1, Image2, Image3, Image4, Image5];
+export default [Image1, Image2, Image3, Image4/*, Image5*/];
