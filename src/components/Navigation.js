ead2eab5d7cdacd767211dcb8b4a232b7f449fba
Eamon Taaffe <eamontaaffe@gmail.com>
2017-05-14 22:04:04 +1000

    flo says she's done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Navigation = () => (
         <div className="Navigation">
         <Link to="/information">Information</Link>
+        <Link to="/" className="back">{"\u2A2F"}</Link>
         </div>
 );
 
