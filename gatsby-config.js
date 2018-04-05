cce49eeb16e8374404dcd95412946ed41d48951c
Eamon Taaffe <eamontaaffe@gmail.com>
2018-04-05 15:00:53 +0200

    Added scss and bourbon neat for grids

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-sass',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 
-import './index.css'
+import './index.scss'
 
 import Header from '../components/Header';
 
