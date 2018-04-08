9c2e74f52b5eb44dce502479d828cfc8f45b8ddd
Florence Li <florenceli@Florences-MBP.gateway>
2018-04-08 15:03:13 +0200

    padding below links

--- src/pages/info.js
+++ src/pages/info.js
@@ -6,12 +6,11 @@ import { MOBILE_MAX_DEVICE_WIDTH } from '../util/config';
 function Content() {
   return (
     <div className="Content">
-      <div className="left">is a Melbourne-born communications designer <a href="http://holidaze.florenceltf.com/">mostly travelling</a> and ocassionally working. Currently freelancing remotely with Melbourne-based studio, <a href="https://www.round.com.au">Round</a>. 
-        <br></br><br></br>
-        Clients worked with while at Round include: Andrew McConnell Group, Australian Institute of Architects, <a href="http://www.thebiggroup.com.au/">The Big Group</a>, Jewish International Film Festival, <a href="https://lovingearth.co/">Loving Earth</a>, Melbourne Food & Wine Festival and <a href="http://siloarttrail.com/">Silo Art Trail</a>.
-        <br></br><br></br>
-        For any portfolio or freelance enquiries, please <a href="mailto:florenceltf@gmail.com">email</a>. For any other enquiries, please still <a href="mailto:florenceltf@gmail.com">email</a>.
-      </div>
+      is a Melbourne-born communications designer <a href="http://holidaze.florenceltf.com/">mostly travelling</a> and ocassionally working. Currently freelancing remotely with Melbourne-based studio, <a href="https://www.round.com.au">Round</a>. 
+      <br></br><br></br>
+      Clients worked with while at Round include: Andrew McConnell Group, Australian Institute of Architects, <a href="http://www.thebiggroup.com.au/">The Big Group</a>, Jewish International Film Festival, <a href="https://lovingearth.co/">Loving Earth</a>, Melbourne Food & Wine Festival and <a href="http://siloarttrail.com/">Silo Art Trail</a>.
+      <br></br><br></br>
+      For any portfolio or freelance enquiries, please <a href="mailto:florenceltf@gmail.com">email</a>. For any other enquiries, please still <a href="mailto:florenceltf@gmail.com">email</a>.
     </div>
   );
 }
